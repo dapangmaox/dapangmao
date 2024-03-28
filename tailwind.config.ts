c0dca431541e1b5090efefe826ffc0be083f5f8e
dapangmaox <oliver.xinwei.liang@gmail.com>
2024-03-28 16:28:42 +0800

    feat: add blog

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,48 @@ const config: Config = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme: any) => ({
+        invert: {
+          css: {
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+            code: {
+              color: theme('colors.gray.100'),
+              backgroundColor: 'transparent',
+              borderRadius: theme('borderRadius.DEFAULT'),
+              paddingLeft: theme('spacing[1]'),
+              paddingRight: theme('spacing[1]'),
+              paddingTop: theme('spacing[0.5]'),
+              paddingBottom: theme('spacing[0.5]'),
+            },
+          },
+        },
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+            code: {
+              color: theme('colors.slate.700'),
+              backgroundColor: theme('colors.stone.100'),
+              borderRadius: theme('borderRadius.DEFAULT'),
+              paddingLeft: theme('spacing[1]'),
+              paddingRight: theme('spacing[1]'),
+              paddingTop: theme('spacing[0.5]'),
+              paddingBottom: theme('spacing[0.5]'),
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
