0709742171faac3d71718b8f6f0a204203c2d5d0
dapangmaox <oliver.xinwei.liang@gmail.com>
2024-07-21 12:12:25 +0800

    feat: remove toc expand, add dark mode for toc

--- app/components/TableOfContents.tsx
+++ app/components/TableOfContents.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import React, { useState } from 'react';
+import React from 'react';
 
 type TocItem = {
   id: string;
@@ -13,34 +11,24 @@ type Props = {
 };
 
 const TableOfContents: React.FC<Props> = ({ toc }) => {
-  const [isExpanded, setIsExpanded] = useState(true);
-
   return (
-    <div className="w-64 sticky top-32 h-96 flex-shrink-0 ml-4 border p-2 rounded">
-      <div className="flex items-center justify-between">
-        <div className="text-slate-500 text-xl">目录</div>
-        <button
-          className="text-sm text-blue-500"
-          onClick={() => setIsExpanded(!isExpanded)}
-        >
-          {isExpanded ? '收起' : '展开'}
-        </button>
+    <div className="w-64 sticky top-32 h-96 flex-shrink-0 ml-4 border pl-2 rounded border-slate-300 dark:border-slate-700">
+      <div className="flex items-center justify-between mt-2">
+        <div className="text-slate-500 dark:text-slate-300 text-xl">目录</div>
       </div>
-      <div className="w-full border-b my-3"></div>
-      {isExpanded && (
-        <div className="overflow-y-auto h-80 text-sm">
-          {toc.map((item) => (
-            <div key={item.id} style={{ marginLeft: `${item.depth - 2}em` }}>
-              <a
-                href={`#${item.id}`}
-                className="no-underline text-slate-500 my-2 block"
-              >
-                {item.title}
-              </a>
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="w-full border-b my-2 border-slate-300 dark:border-slate-700"></div>
+      <div className="overflow-y-auto h-80 text-sm">
+        {toc.map((item) => (
+          <div key={item.id} style={{ marginLeft: `${item.depth - 2}em` }}>
+            <a
+              href={`#${item.id}`}
+              className="no-underline text-slate-500 dark:text-slate-300 my-2 block"
+            >
+              {item.title}
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
