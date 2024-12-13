2cbd5aa71a7a7dc9ad7ad1cfa480a5b600c6c67a
dapangmaox <oliver.xinwei.liang@gmail.com>
2024-12-13 16:53:24 +0800

    style: update main content margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning={true}>
-      <body className="text-slate-500 dark:text-slate-400 bg-white dark:bg-gray-900">
+      <body className="text-slate-500 dark:text-slate-400 bg-white dark:bg-gray-900 flex flex-col items-center">
         <ThemeProvider>
           <Header />
-          <main className="pt-16 sm:mx-auto mx-4 max-w-3xl prose dark:prose-invert">
+          <main className="pt-16 px-4 sm:px-8 w-full max-w-3xl prose dark:prose-invert">
             {children}
           </main>
         </ThemeProvider>
