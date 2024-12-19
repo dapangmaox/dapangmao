c4d1be4d620dc4a0527e423bfe6dfbedf655aa12
dapangmaox <oliver.xinwei.liang@gmail.com>
2024-12-19 17:52:41 +0800

    feat: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    MY_SECRET_TOKEN: process.env.MY_SECRET_TOKEN,
+    MY_GITHUB_TOKEN: process.env.MY_GITHUB_TOKEN,
+  },
+};
 
 export default nextConfig;
